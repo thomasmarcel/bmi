c09942e053af75d2e5a15ce46c40a4f681d9572c
Thomas Alcala <thomas.alcala@gmail.com>
2017-07-14 04:04:58 -0500

    Add some simple tests for to check index and home routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'home', to: 'pages#home'
+  get 'home', to: 'pages#home', as: :home
 
   root to: "pages#index"
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
-class PagesControllerTest < ActionDispatch::IntegrationTest
+class PagesControllerTest < ActionController::TestCase
+# class PagesControllerTest < ActionDispatch::IntegrationTest
+
+  include Devise::Test::ControllerHelpers
+
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get home" do
+    sign_in @user
+    get :home
+    assert_response :success
+  end
 end
